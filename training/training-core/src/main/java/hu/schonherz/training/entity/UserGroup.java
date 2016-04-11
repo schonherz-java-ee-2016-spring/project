6b37ded7d5fb8ff45ac35a30a5aef9ce17a78c72
Kovács Szabolcs <montuV9@gmail.com>
2016-04-11 23:31:40 +0200

    UserGroupRepository.java

--- training/training-core/src/main/java/hu/schonherz/training/entity/UserGroup.java
+++ training/training-core/src/main/java/hu/schonherz/training/entity/UserGroup.java
@@ -1,6 +1,13 @@
 package hu.schonherz.training.entity;
 
+import java.util.Collection;
+import java.util.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -10,13 +17,35 @@ public class UserGroup extends BaseEntity {
 	private static final long serialVersionUID = 1L;
 
 	private String groupName;
-
+	
+	private Date creaationDate;
+	
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "roleGroup_to_userGroup", joinColumns = @JoinColumn(name = "userGroup_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "roleGroup_id", referencedColumnName = "id"))
+	private Collection<RoleGroup> roleGroups;
+	
 	public String getGroupName() {
 		return groupName;
 	}
 
 	public void setGroupName(String groupName) {
 		this.groupName = groupName;
+	}
+
+	public Date getCreaationDate() {
+		return creaationDate;
+	}
+
+	public void setCreaationDate(Date creaationDate) {
+		this.creaationDate = creaationDate;
+	}
+
+	public Collection<RoleGroup> getRoleGroups() {
+		return roleGroups;
+	}
+
+	public void setRoleGroups(Collection<RoleGroup> roleGroups) {
+		this.roleGroups = roleGroups;
 	}
 
 	
