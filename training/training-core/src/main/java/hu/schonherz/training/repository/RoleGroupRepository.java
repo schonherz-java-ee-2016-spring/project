b1c8a289eb6efc6cec46e38b68ae8896419972f0
Magyari Norbert <magyari.mn@gmail.com>
2016-04-11 21:48:52 +0200

    hotfix

--- training/training-core/src/main/java/hu/schonherz/training/repository/RoleGroupRepository.java
+++ training/training-core/src/main/java/hu/schonherz/training/repository/RoleGroupRepository.java
@@ -1,9 +1,6 @@
 package hu.schonherz.training.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +12,5 @@ import hu.schonherz.training.entity.RoleGroup;
 @Transactional(propagation = Propagation.REQUIRED)
 public interface RoleGroupRepository extends JpaRepository<RoleGroup, Long> {
 
-	@Query("select roles from roleGroup g join g.roles roles where g.id=?1")
-	List<RoleGroup> findRolesByGroupId(Long groupId);
+	
 }
