4243844e116fe19a1cf9e75b23542870714c1b47
Ölveti József <olvjoz@gmail.com>
2016-04-11 23:11:54 +0200

    role entity

--- training/training-core/src/main/java/hu/schonherz/training/entity/Role.java
+++ training/training-core/src/main/java/hu/schonherz/training/entity/Role.java
@@ -1,5 +1,6 @@
 package hu.schonherz.training.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -9,6 +10,7 @@ public class Role extends BaseEntity {
 
 	private static final long serialVersionUID = 1L;
 
+	@Column(nullable = false, unique = true)
 	private String name;
 
 	public String getName() {
