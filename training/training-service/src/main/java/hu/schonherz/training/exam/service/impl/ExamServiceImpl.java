b4c67dd1777d5866c9103c2aafbef9944457c43e
Iványi-Nagy Gábor <gabilampard@gmail.com>
2016-04-12 21:42:12 +0200

    add logger to service/impl/ExamServiceImpl.java, service/impl/QuestionTypeServiceImpl.java add log4j maven dependency

--- training/training-service/src/main/java/hu/schonherz/training/exam/service/impl/ExamServiceImpl.java
+++ training/training-service/src/main/java/hu/schonherz/training/exam/service/impl/ExamServiceImpl.java
@@ -1,25 +1,39 @@
 package hu.schonherz.training.exam.service.impl;
 
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+import javax.interceptor.Interceptors;
+import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ejb.interceptor.SpringBeanAutowiringInterceptor;
 
 import hu.schonherz.training.exam.repository.ExamRepository;
 import hu.schonherz.training.exam.service.ExamService;
 import hu.schonherz.training.exam.service.mapper.ExamMapper;
 import hu.schonherz.training.exam.vo.ExamVo;
 
-@Service
-@Transactional(propagation = Propagation.REQUIRED)
+@Stateless(mappedName = "ExamService", name = "ExamService")
+@Transactional(value = TxType.REQUIRED)
+@Local(ExamService.class)
+@Interceptors({ SpringBeanAutowiringInterceptor.class })
 public class ExamServiceImpl implements ExamService {
+
+	static final Logger logger = LogManager.getLogger(ExamServiceImpl.class.getName());
 
 	@Autowired
 	ExamRepository examRepository;
 
 	@Override
 	public void createExam(ExamVo examVo) throws Exception {
-		examRepository.saveAndFlush(ExamMapper.toDto(examVo));
-		
+		try {
+			examRepository.saveAndFlush(ExamMapper.toDto(examVo));
+		} catch (Exception ex) {
+			logger.error(ex.getMessage(), ex);
+		}
+
 	}
 }
