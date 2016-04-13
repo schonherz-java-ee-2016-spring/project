27d1dc24f22eed95bbb02661bf6d6d5b808d8f68
Iványi-Nagy Gábor <gabilampard@gmail.com>
2016-04-13 21:38:42 +0200

    Change name to title...

--- training/training-service-client-api/src/main/java/hu/schonherz/training/exam/vo/ExamVo.java
+++ training/training-service-client-api/src/main/java/hu/schonherz/training/exam/vo/ExamVo.java
@@ -8,7 +8,7 @@ public class ExamVo implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
-	private String name;
+	private String title;
 	private List<QuestionVo> questionList;
 
 	public ExamVo() {
@@ -23,12 +23,12 @@ public class ExamVo implements Serializable {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getTitle() {
+		return title;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setTitle(String title) {
+		this.title = title;
 	}
 
 	public List<QuestionVo> getQuestionList() {
