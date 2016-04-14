15f53b6b12da99413b300ffaf633bb7bf9e9b6af
Naményi János <namenyi.janos@gmail.com>
2016-04-14 17:49:54 +0200

    ***Fixed Interview service interface methods (some methods had incorrect return type) ***Added methods to Feedback service interface (for get the feedbacks) ***Created mappers for Feedback and Interview

--- training/training-service-client-api/src/main/java/hu/schonherz/training/supervisor/service/FeedbackService.java
+++ training/training-service-client-api/src/main/java/hu/schonherz/training/supervisor/service/FeedbackService.java
@@ -1,9 +1,14 @@
 package hu.schonherz.training.supervisor.service;
 
+import java.util.List;
+
 import hu.schonherz.training.supervisor.vo.FeedbackVo;
 
 public interface FeedbackService {
 
 	public FeedbackVo giveFeedback(String ratedName) throws Exception;
 	
+	public FeedbackVo getFeedback(Long interviewId) throws Exception;
+	
+	public List<FeedbackVo> getAllFeedbackByUser(Long userId) throws Exception;
 }
--- training/training-service-client-api/src/main/java/hu/schonherz/training/supervisor/service/InterviewService.java
+++ training/training-service-client-api/src/main/java/hu/schonherz/training/supervisor/service/InterviewService.java
@@ -1,6 +1,7 @@
 package hu.schonherz.training.supervisor.service;
 
 import java.util.Date;
+import java.util.List;
 
 import hu.schonherz.training.supervisor.vo.InterviewVo;
 
@@ -8,9 +9,11 @@ public interface InterviewService {
 
 	public InterviewVo addInterview(InterviewVo interviewVo) throws Exception;
 	
-	public InterviewVo findAllByUser(Long userId) throws Exception;
+	public InterviewVo getInterview(Long interviewId) throws Exception;
 	
-	public InterviewVo findAllByUserAndDate(Long userId, Date interviewDate) throws Exception;
+	public List<InterviewVo> getAllByUser(Long userId) throws Exception;
+	
+	public List<InterviewVo> getAllByUserAndDate(Long userId, Date interviewDate) throws Exception;
 	
 	public void deleteInterview(Long interviewId) throws Exception;
 	
