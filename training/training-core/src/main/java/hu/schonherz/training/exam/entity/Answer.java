3fcb9bc64ad6c32fe0e4affb983f3b14e5ce7d45
Fekete Attila <atkavagyok@gmail.com>
2016-04-15 21:48:10 +0200

    Comments added to exam entities

--- training/training-core/src/main/java/hu/schonherz/training/exam/entity/Answer.java
+++ training/training-core/src/main/java/hu/schonherz/training/exam/entity/Answer.java
@@ -10,19 +10,42 @@ import javax.persistence.Table;
 import hu.schonherz.training.entity.BaseEntity;
 import hu.schonherz.training.entity.User;
 
+/**
+ * The database entity of an Answer
+ * 
+ * This entity works like a relational table to connect a {@link User} with an
+ * {@link Option} he chose.
+ */
 @Entity
 @Table(name = "answer")
 public class Answer extends BaseEntity {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * The {@link User} who submitted the Answer
+	 * 
+	 * Represented as {@code user_id} in the database
+	 */
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id", referencedColumnName = "id")
 	private User user;
 
+	/**
+	 * The {@link Option} that was submitted
+	 * 
+	 * Represented as {@code option_id} in the database
+	 */
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "option_id", referencedColumnName = "id")
 	private Option option;
 
+	/**
+	 * The result of the Answer
+	 * 
+	 * It is usually the same as the option's {@code correct} field, but if the
+	 * Question's type is text-based, the Answer is not an Option that can be
+	 * evaluated immediately, it must be approved by an Instructor
+	 */
 	@Column(name = "good", nullable = true)
 	private Boolean good;
 
@@ -30,28 +53,49 @@ public class Answer extends BaseEntity {
 		super();
 	}
 
-	public Boolean isGood() {
-		return good;
-	}
-
-	public void setGood(Boolean good) {
-		this.good = good;
-	}
-
+	/**
+	 * @return the user
+	 */
 	public User getUser() {
 		return user;
 	}
 
+	/**
+	 * @param user
+	 *            the user to set
+	 */
 	public void setUser(User user) {
 		this.user = user;
 	}
 
+	/**
+	 * @return the option
+	 */
 	public Option getOption() {
 		return option;
 	}
 
+	/**
+	 * @param option
+	 *            the option to set
+	 */
 	public void setOption(Option option) {
 		this.option = option;
+	}
+
+	/**
+	 * @return the good
+	 */
+	public Boolean isGood() {
+		return good;
+	}
+
+	/**
+	 * @param good
+	 *            the good to set
+	 */
+	public void setGood(Boolean good) {
+		this.good = good;
 	}
 
 }
--- training/training-core/src/main/java/hu/schonherz/training/exam/entity/AnswerNote.java
+++ training/training-core/src/main/java/hu/schonherz/training/exam/entity/AnswerNote.java
@@ -8,39 +8,64 @@ import javax.persistence.Table;
 
 import hu.schonherz.training.entity.BaseEntity;
 
+/**
+ * The database entity of an Answer note
+ * 
+ * These notes are written by the Instructor to comment your {@link Answer}
+ */
 @Entity
 @Table(name = "answer_note")
 public class AnswerNote extends BaseEntity {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * The {@link Answer} which has the note attached to it
+	 * 
+	 * Represented as {@code answer_id} in the database
+	 */
 	@OneToOne
 	@JoinColumn(name = "answer_id", referencedColumnName = "id")
 	private Answer answer;
 
+	/**
+	 * The note that contains the message
+	 */
 	@Column(nullable = true)
-
-//	@Type(type = "org.hibernate.type.StringClobType")
-//	@Lob
+	// @Type(type = "org.hibernate.type.StringClobType")
+	// @Lob
 	private String note;
 
 	public AnswerNote() {
 		super();
 	}
 
-	public String getNote() {
-		return note;
-	}
-
-	public void setNote(String note) {
-		this.note = note;
-	}
-
+	/**
+	 * @return the answer
+	 */
 	public Answer getAnswer() {
 		return answer;
 	}
 
+	/**
+	 * @param answer
+	 *            the answer to set
+	 */
 	public void setAnswer(Answer answer) {
 		this.answer = answer;
 	}
 
+	/**
+	 * @return the note
+	 */
+	public String getNote() {
+		return note;
+	}
+
+	/**
+	 * @param note
+	 *            the note to set
+	 */
+	public void setNote(String note) {
+		this.note = note;
+	}
 }
