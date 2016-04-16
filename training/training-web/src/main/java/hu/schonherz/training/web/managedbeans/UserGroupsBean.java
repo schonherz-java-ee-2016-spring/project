10f756770a578e40a24dc578b708297e6d41e184
Ölveti József <olvjoz@gmail.com>
2016-04-16 11:16:16 +0200

    user groups better,better,better implementation, one dialog

--- training/training-web/src/main/java/hu/schonherz/training/web/managedbeans/UserGroupsBean.java
+++ training/training-web/src/main/java/hu/schonherz/training/web/managedbeans/UserGroupsBean.java
@@ -27,11 +27,7 @@ public class UserGroupsBean implements Serializable {
 	private List<UserGroupVo> userGroups;
 
 	private UserGroupVo selected;
-
-	private String groupName;
-	private String description;
-
-	private Boolean isSelected = true;
+	private Boolean isDisabled = true;
 
 	@PostConstruct
 	public void init() {
@@ -44,26 +40,34 @@ public class UserGroupsBean implements Serializable {
 	}
 
 	public void selectGroupListener(SelectEvent event) {
-		isSelected = false;
+		isDisabled = false;
 	}
 
-	public void create() {
+	public void createAction() {
+		selected = new UserGroupVo();
+	}
+
+	public void save() {
+		Long isCreateAction = selected.getId();
 		try {
-			if (userGroupService.findGroupByName(groupName) != null) {
+			UserGroupVo gvo = userGroupService.findGroupByName(selected.getGroupName());
+			if ((gvo != null) && !gvo.getId().equals(selected.getId())) {
 				FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "WARNING",
-						"User Group already exists!");
+						"User Group name is already exists!");
 				FacesContext.getCurrentInstance().addMessage(null, msg);
 			} else {
-				UserGroupVo gvo = new UserGroupVo();
-				gvo.setName(groupName);
-				gvo.setDescription(description);
-				userGroupService.saveUserGroup(gvo);
-				userGroups.add(userGroupService.findGroupByName(gvo.getGroupName()));
-				FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "SUCCESS", "User Group created!");
+				userGroupService.saveUserGroup(selected);
+				userGroups.remove(selected);
+				selected = userGroupService.findGroupByName(selected.getGroupName());
+				userGroups.add(selected);
+				FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "SUCCESS", "User Group saved!");
 				FacesContext.getCurrentInstance().addMessage(null, msg);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+		if (isCreateAction == null) {
+			selected = new UserGroupVo();
 		}
 	}
 
@@ -71,26 +75,6 @@ public class UserGroupsBean implements Serializable {
 		try {
 			userGroupService.deleteUserGroup(selected.getId());
 			userGroups.remove(selected);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void updateGroup() {
-		try {
-			UserGroupVo gvo = userGroupService.findGroupByName(selected.getGroupName());
-			if ((gvo != null) && !gvo.getId().equals(selected.getId())) {
-				FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "WARNING",
-						"User Group already exists!");
-				FacesContext.getCurrentInstance().addMessage(null, msg);
-			} else {
-				userGroupService.saveUserGroup(selected);
-				gvo = userGroupService.getUserGroupById(selected.getId());
-				selected.setModUser(gvo.getModUser());
-				selected.setModDate(gvo.getModDate());
-				FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "SUCCESS", "User Group updated!");
-				FacesContext.getCurrentInstance().addMessage(null, msg);
-			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -112,28 +96,11 @@ public class UserGroupsBean implements Serializable {
 		this.selected = selected;
 	}
 
-	public Boolean getIsSelected() {
-		return isSelected;
+	public Boolean getIsDisabled() {
+		return isDisabled;
 	}
 
-	public void setIsSelected(Boolean isSelected) {
-		this.isSelected = isSelected;
+	public void setIsDisabled(Boolean isDisabled) {
+		this.isDisabled = isDisabled;
 	}
-
-	public String getGroupName() {
-		return groupName;
-	}
-
-	public void setGroupName(String groupName) {
-		this.groupName = groupName;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
 }
