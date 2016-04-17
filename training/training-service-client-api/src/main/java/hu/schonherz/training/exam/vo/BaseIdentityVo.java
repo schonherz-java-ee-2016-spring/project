01f090cd6f251cb1815bba233aae883cae3fe295
Fekete Attila <atkavagyok@gmail.com>
2016-04-17 17:41:00 +0200

    Cleaning the Exam UI

--- training/training-service-client-api/src/main/java/hu/schonherz/training/exam/vo/BaseIdentityVo.java
+++ training/training-service-client-api/src/main/java/hu/schonherz/training/exam/vo/BaseIdentityVo.java
@@ -1,11 +1,16 @@
 package hu.schonherz.training.exam.vo;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class BaseIdentityVo implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	String recUser;
+	String modUser;
+	Date recDate;
+	Date modDate;
 
 	public BaseIdentityVo() {
 		super();
@@ -17,6 +22,38 @@ public class BaseIdentityVo implements Serializable {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getRecUser() {
+		return recUser;
+	}
+
+	public void setRecUser(String recUser) {
+		this.recUser = recUser;
+	}
+
+	public String getModUser() {
+		return modUser;
+	}
+
+	public void setModUser(String modUser) {
+		this.modUser = modUser;
+	}
+
+	public Date getRecDate() {
+		return recDate;
+	}
+
+	public void setRecDate(Date recDate) {
+		this.recDate = recDate;
+	}
+
+	public Date getModDate() {
+		return modDate;
+	}
+
+	public void setModDate(Date modDate) {
+		this.modDate = modDate;
 	}
 
 }
