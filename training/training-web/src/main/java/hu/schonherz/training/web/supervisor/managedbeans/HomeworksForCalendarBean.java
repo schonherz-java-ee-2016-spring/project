e27d1c372c6752f5164e7265753476b1873ea967
Bohán Márk <bmark13@vipmail.hu>
2016-04-23 14:55:06 +0200

    latest, still not working

---
+++ training/training-web/src/main/java/hu/schonherz/training/web/supervisor/managedbeans/HomeworksForCalendarBean.java
@@ -0,0 +1,57 @@
+package hu.schonherz.training.web.supervisor.managedbeans;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import hu.schonherz.training.service.supervisor.HomeworkService;
+import hu.schonherz.training.service.supervisor.vo.HomeworkVo;
+
+@ManagedBean(name = "homeworksForCalendarBean")
+@ViewScoped
+public class HomeworksForCalendarBean implements Serializable {
+
+	private static final long serialVersionUID = 203865612177768480L;
+
+	@EJB
+	private HomeworkService homeworkService;
+
+	private List<HomeworkVo> homeworks;
+
+	
+	public HomeworksForCalendarBean() {
+
+		init();
+	}
+	@PostConstruct
+	public void init() {
+		try {
+			// homeworks = homeworkService.getAllHomeworks();
+			homeworks = new ArrayList<>();
+			HomeworkVo homework = new HomeworkVo();
+			homework.setDeadline(new GregorianCalendar().getTime());
+			homework.setId((long) 0);
+			homework.setDescription("asdf asd");
+			homework.setTitle("yolo");
+
+			homeworks.add(homework);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public List<HomeworkVo> getHomeworks() {
+		return homeworks;
+	}
+
+	public void setHomeworks(List<HomeworkVo> homeworks) {
+		this.homeworks = homeworks;
+	}
+
+}
