dd89e3578be7fb673f21bcb6a325928ade0a861f
Iványi-Nagy Gábor <gabilampard@gmail.com>
2016-04-30 14:44:35 +0200

    Done examReview for Student

---
+++ training/training-web/src/main/java/hu/schonherz/training/web/exam/managedbeans/ExamReviewForInstructorBean.java
@@ -0,0 +1,11 @@
+package hu.schonherz.training.web.exam.managedbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name = "examReviewForInstructorBean")
+@ViewScoped
+public class ExamReviewForInstructorBean extends ExamReviewBean {
+	private static final long serialVersionUID = 1L;
+	
+}
