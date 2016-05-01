02e0c33de77450de9480ba134997d81728f83d52
Preznyák László <laszlopreznyak11@gmail.com>
2016-05-01 14:36:03 +0200

    #64 Profile Overview - Create necessary UI components - UserToProfile Bean Added

---
+++ training/training-web/src/main/java/hu/schonherz/training/web/supervisor/managedbeans/MBUserToProfile.java
@@ -0,0 +1,52 @@
+package hu.schonherz.training.web.supervisor.managedbeans;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import hu.schonherz.training.service.admin.UserService;
+import hu.schonherz.training.service.admin.vo.UserVo;
+
+@ManagedBean(name = "profileBean")
+@ViewScoped
+public class MBUserToProfile implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private UserVo user;
+
+	@EJB
+	private UserService userService;
+
+	@PostConstruct
+	public void init() {
+
+		String username = FacesContext.getCurrentInstance().getExternalContext().getRemoteUser();
+		try {
+			user = userService.findUserByName(username);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public MBUserToProfile() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserVo getUser() {
+		return user;
+	}
+
+	public void setUser(UserVo user) {
+		this.user = user;
+	}
+
+}
