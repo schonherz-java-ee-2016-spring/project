9bcbd42c93fa41f0dc644f8cf1f9a2f94c53e85e
Erdei Krisztián <headswitcher@gmail.com>
2016-05-02 11:23:49 +0200

    Last comit + this

---
+++ training/training-service/src/test/java/hu/schonherz/training/service/exam/test/QuestionTypeServiceTest.java
@@ -0,0 +1,84 @@
+package hu.schonherz.training.service.exam.test;
+
+import java.util.List;
+
+import javax.annotation.ManagedBean;
+import javax.ejb.EJB;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import hu.schonherz.training.service.exam.QuestionTypeService;
+import hu.schonherz.training.service.exam.vo.ExamVo;
+import hu.schonherz.training.service.exam.vo.QuestionTypeVo;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@ManagedBean
+public class QuestionTypeServiceTest {
+
+	static final Logger logger = LogManager.getLogger(QuestionServiceTest.class.getName());
+
+	
+	@EJB
+	QuestionTypeService questionTypeService;
+
+	@Before
+	public void startTheContainer() throws Exception {
+		try {
+			CreateContext.ejbContainer.getContext().bind("inject", this);
+		} catch (Throwable ex) {
+			logger.error(ex.getMessage(), ex);
+		}
+		
+		QuestionTypeVo questionTypeVo= new QuestionTypeVo();
+		questionTypeVo.setName("Teszt");
+		questionTypeService.add(questionTypeVo);
+		
+	}
+	
+	@After
+	public void tearDown() {
+		try {
+			QuestionTypeVo questionTypeVo= questionTypeService.getByName("Teszt");
+			questionTypeService.removeById(questionTypeVo.getId());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test(expected = Exception.class)
+	public void saveTestWithFail() throws Exception {
+	QuestionTypeVo questionTypeVo = new QuestionTypeVo();
+		questionTypeService.add(questionTypeVo);
+	}
+	
+	@Test(expected = Exception.class)
+	public void removeByIdTestWithFail() throws Exception {
+		questionTypeService.removeById(-1L);
+	}
+	
+
+	@Test
+	public void getAllTest() {
+		
+	}
+	
+	@Test(expected = Exception.class)
+	public void getAllTestWithFail() {
+		try {
+			List<QuestionTypeVo> questionTypeVos = questionTypeService.getAll();
+			
+			Assert.assertEquals(true, (questionTypeVos == null ? false : true));
+		} catch (Exception ex) {
+			logger.error(ex.getMessage(), ex);
+		}
+	}
+	
+
+}
