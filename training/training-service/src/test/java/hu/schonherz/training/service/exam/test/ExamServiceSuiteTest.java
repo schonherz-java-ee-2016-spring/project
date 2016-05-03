6b95a1ad04cb8580208743fc79cd11fdecf94ace
Iványi-Nagy Gábor <gabilampard@gmail.com>
2016-05-03 15:16:09 +0200

    Fix it tests

--- training/training-service/src/test/java/hu/schonherz/training/service/exam/test/ExamServiceSuiteTest.java
+++ training/training-service/src/test/java/hu/schonherz/training/service/exam/test/ExamServiceSuiteTest.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ CreateContext.class, QuestionServiceTest.class, CloseContext.class })
-public class QuestionServiceSuite {
+@SuiteClasses({ CreateContext.class, ExamServiceTest.class, CloseContext.class })
+public class ExamServiceSuiteTest {
 
 }
