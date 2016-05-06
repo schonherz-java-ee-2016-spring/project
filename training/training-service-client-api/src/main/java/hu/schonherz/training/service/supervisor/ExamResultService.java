e20be08b922e93e7ca78c4b79b671b219ebad000
Preznyák László <laszlopreznyak11@gmail.com>
2016-05-06 12:41:09 +0200

    #56 Result Overview - Create initial data - ExamResultService.java created

---
+++ training/training-service-client-api/src/main/java/hu/schonherz/training/service/supervisor/ExamResultService.java
@@ -0,0 +1,16 @@
+package hu.schonherz.training.service.supervisor;
+
+import java.util.List;
+
+import hu.schonherz.training.service.admin.vo.UserVo;
+import hu.schonherz.training.service.exam.vo.ExamVo;
+import hu.schonherz.training.service.supervisor.vo.ExamResultVo;
+
+public interface ExamResultService {
+
+	public List<ExamResultVo> getAll() throws Exception;
+
+	public List<ExamResultVo> getExamResultByUser(UserVo user) throws Exception;
+
+	public List<ExamResultVo> getExamResultByExam(ExamVo exam) throws Exception;
+}
