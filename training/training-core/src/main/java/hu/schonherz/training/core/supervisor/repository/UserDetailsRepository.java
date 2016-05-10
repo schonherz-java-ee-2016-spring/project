0c8f725d9c5655a179f6f098c75036ac79029f31
Preznyák László <laszlopreznyak11@gmail.com>
2016-05-10 16:03:07 +0200

    #63 Profile Overview - Create initial data - Add Personal data - new database table - UserDetailsRepository.java added

---
+++ training/training-core/src/main/java/hu/schonherz/training/core/supervisor/repository/UserDetailsRepository.java
@@ -0,0 +1,19 @@
+package hu.schonherz.training.core.supervisor.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import hu.schonherz.training.core.admin.entity.User;
+import hu.schonherz.training.core.supervisor.entity.UserDetails;
+
+@Repository
+@Transactional(propagation = Propagation.SUPPORTS)
+public interface UserDetailsRepository extends JpaRepository<UserDetails, Long> {
+
+	public UserDetails findUserDetailsByUser(User user);
+
+	public UserDetails findUserDetailsByUserId(Long userId);
+
+}
