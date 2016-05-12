7ef064321743941352819353a02d3d90c638a68b
Preznyák László <laszlopreznyak11@gmail.com>
2016-05-12 17:41:18 +0200

    REsults and Profile changed. Users ordered, data changeable.

--- training/training-web/src/main/java/hu/schonherz/training/web/supervisor/managedbeans/MBResultsBean.java
+++ training/training-web/src/main/java/hu/schonherz/training/web/supervisor/managedbeans/MBResultsBean.java
@@ -2,14 +2,15 @@ package hu.schonherz.training.web.supervisor.managedbeans;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import hu.schonherz.training.service.admin.ThemeService;
 import hu.schonherz.training.service.admin.TrainingService;
@@ -23,6 +24,7 @@ import hu.schonherz.training.service.supervisor.vo.ExamResultVo;
 import hu.schonherz.training.service.supervisor.vo.HomeworkResultVo;
 import hu.schonherz.training.web.supervisor.accessories.Course;
 import hu.schonherz.training.web.supervisor.accessories.UserResults;
+import hu.schonherz.training.web.supervisor.accessories.UserResultsComparator;
 
 @ManagedBean(name = "resultsBean", eager = true)
 @ViewScoped
@@ -49,10 +51,17 @@ public class MBResultsBean implements Serializable {
 	// List of Courses
 	private List<Course> courses = new ArrayList<>();
 
+	private UserVo loggedInUser;
+
 	// init method for resultsBean
 	@PostConstruct
 	public void init() {
-
+		String username = FacesContext.getCurrentInstance().getExternalContext().getRemoteUser();
+		try {
+			loggedInUser = userService.findUserByName(username);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		List<TrainingVo> trainings = new ArrayList<TrainingVo>();
 		try {
 			trainings = trainingService.getAllTrainings();
@@ -109,6 +118,7 @@ public class MBResultsBean implements Serializable {
 				userResult.setExamSum(examSum);
 				userResult.setHomeworkSum(homeworkSum);
 			}
+			Collections.sort(course.getUserResults(), new UserResultsComparator());
 		}
 	}
 
@@ -127,6 +137,14 @@ public class MBResultsBean implements Serializable {
 
 	public void setCourses(List<Course> courses) {
 		this.courses = courses;
+	}
+
+	public UserVo getLoggedInUser() {
+		return loggedInUser;
+	}
+
+	public void setLoggedInUser(UserVo loggedInUser) {
+		this.loggedInUser = loggedInUser;
 	}
 
 }
