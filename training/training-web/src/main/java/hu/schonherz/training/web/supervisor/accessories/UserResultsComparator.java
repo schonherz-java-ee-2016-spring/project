a1a41aac7cb6cc01b0072019cf8f423a2ee77ad2
Janos Pelsoczi <Janos.Pelsoczi@SurveySampling.com>
2016-05-13 21:09:47 +0200

    Extreme Programming with Laci fix ordering issue and show sum points

--- training/training-web/src/main/java/hu/schonherz/training/web/supervisor/accessories/UserResultsComparator.java
+++ training/training-web/src/main/java/hu/schonherz/training/web/supervisor/accessories/UserResultsComparator.java
@@ -6,8 +6,8 @@ public class UserResultsComparator implements Comparator<UserResults> {
 
 	@Override
 	public int compare(UserResults o1, UserResults o2) {
-		return (o1.getExamSum() + o1.getHomeworkSum()) > (o2.getExamSum() + o2.getHomeworkSum()) ? 1
-				: (o1.getExamSum() + o1.getHomeworkSum()) == (o2.getExamSum() + o2.getHomeworkSum()) ? 0 : -1;
+		return (o1.getExamSum() + o1.getHomeworkSum()) > (o2.getExamSum() + o2.getHomeworkSum()) ? -1
+				: (o1.getExamSum() + o1.getHomeworkSum()) == (o2.getExamSum() + o2.getHomeworkSum()) ? 0 : 1;
 	}
 
 }
